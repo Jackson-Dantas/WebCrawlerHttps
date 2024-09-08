d002db9a801e04f1d33265b22593add7d4470e04
Jackson-Dantas <jacsdaniel40@gmail.com>
2024-09-08 17:20:48 -0300

    getURLsFromHTML funciton and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,35 @@
+const { url } = require('inspector');
+const {JSDOM} = require('jsdom');
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []; 
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a');
+    
+    linkElements.forEach(link => {
+        
+        if(link.href.slice(0,1) === '/'){
+            try{
+                const urlObj = new URL(`${baseURL}${link.href}`)
+                urls.push(urlObj.href)
+            }catch(err){
+                console.log(`error with relative url: ${err.message}`)
+            }   
+        }else{
+            try{
+                const urlObj = new URL(link.href)
+                urls.push(urlObj.href)
+            }catch(err){
+                console.log(`error with absolute url: ${err.message}`)
+            }   
+        }     
+    
+    });
+
+    return urls; 
+}
+
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString);
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -10,5 +42,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
